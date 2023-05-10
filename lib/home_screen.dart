c0f201a63381651c1fa7b8242c8120197bcba2ef
Zohaib234 <zhbah234@gmail.com>
2023-05-10 19:11:45 +0500

    Drawer is added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,6 +16,46 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("navigation "),
         backgroundColor: Colors.teal,
       ),
+      drawer: Drawer(
+        child:  ListView(
+          padding: EdgeInsets.zero,
+          children:   [
+           const  UserAccountsDrawerHeader(accountName: Text("Zohaib Ahmed"), accountEmail: Text("zhbah234@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage("https://images.unsplash.com/photo-1594751543129-6701ad444259?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZGFyayUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80"),
+              ),
+             decoration: BoxDecoration(
+               color: Colors.teal
+             ),
+            ),
+           ListTile(
+             leading:  const Icon(Icons.home),
+             title:  const Text("page1"),
+             onTap: (){
+               Navigator.push(context , MaterialPageRoute(builder: (context)=>ScreenTwo()));
+               },
+
+           ),
+            ListTile(
+              leading:  const Icon(Icons.cleaning_services),
+              title:  const Text("page2"),
+              onTap: (){
+
+              },
+
+            ),
+            ListTile(
+              leading:  const Icon(Icons.logout_outlined),
+              title:  const Text("Log out"),
+              onTap: (){
+
+              },
+
+            ),
+
+          ],
+        ),
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
